48e394678a4f42d8a413ddbf22c1db400484b5d8
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 10:14:07 +0100

    Task/main view setup (#2)
    * add background color to main view
    
    * Add table view to MainListViewController
    
    Introduces a UITableView to MainListViewController with basic setup, constraints, and dummy data. Implements UITableViewDelegate and UITableViewDataSource to display 10 rows with their index as text.
    
    * Move delegate files to Delegates directory
    
    Renamed AppDelegate.swift and SceneDelegate.swift from ViewModels/Delegates to Delegates for improved project structure and organization.

--- pixel-stackoverflow-list/Views/ViewController.swift
+++ pixel-stackoverflow-list/Views/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class MainListViewController: UIViewController {
+final class MainListViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableView.automaticDimension
+        return tableView
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
 
 }
 
+private extension MainListViewController {
+    func setup() {
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension MainListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
