81ce8e9c01fce12e60a3073125ba72b7cc1821c4
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 12:13:15 +0100

    Create UsersResponse.swift

---
+++ pixel-stackoverflow-list/Models/UsersResponse.swift
@@ -0,0 +1,12 @@
+//
+//  UsersResponse.swift
+//  pixel-stackoverflow-list
+//
+//  Created by Aleksandrs Trubacs on 05/09/2025.
+//
+
+import Foundation
+
+struct UsersResponse: Codable {
+    public let items: [User]
+}
