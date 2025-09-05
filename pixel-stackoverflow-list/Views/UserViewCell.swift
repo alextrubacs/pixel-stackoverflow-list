6369a8245893669d222191b1410b8c23b8adb141
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 10:22:41 +0100

    Create UserViewCell.swift

---
+++ pixel-stackoverflow-list/Views/UserViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserViewCell.swift
+//  pixel-stackoverflow-list
+//
+//  Created by Aleksandrs Trubacs on 05/09/2025.
+//
+
+import UIKit
+
+class UserViewCell: UICollectionViewCell {
+    
+}
