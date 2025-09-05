fe21240404cba5c7d3ce14df0a54d76d196a089f
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 10:33:17 +0100

    Create User.swift

---
+++ pixel-stackoverflow-list/Models/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  pixel-stackoverflow-list
+//
+//  Created by Aleksandrs Trubacs on 05/09/2025.
+//
+
+import Foundation
+
+struct UsersResponse: Codable {
+    public let items: [User]
+}
+
+struct User: Codable {
+    let accountId: Int?
+    let reputation: Int
+    let userType: String
+    let userId: Int
+    let location: String?
+    let profileImage: URL?
+    let displayName: String
+
+    enum CodingKeys: String, CodingKey {
+        case accountId = "account_id"
+        case reputation
+        case userType = "user_type"
+        case userId = "user_id"
+        case location
+        case profileImage = "profile_image"
+        case displayName = "display_name"
+    }
+}
