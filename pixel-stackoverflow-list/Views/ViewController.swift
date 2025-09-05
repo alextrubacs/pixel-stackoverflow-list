e3baa4e149518a6dfedfce7a9722f0b7e6b2207f
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 10:11:14 +0100

    Add table view to MainListViewController
    Introduces a UITableView to MainListViewController with basic setup, constraints, and dummy data. Implements UITableViewDelegate and UITableViewDataSource to display 10 rows with their index as text.

--- pixel-stackoverflow-list/Views/ViewController.swift
+++ pixel-stackoverflow-list/Views/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class MainListViewController: UIViewController {
+final class MainListViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableView.automaticDimension
+        return tableView
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
 
@@ -19,6 +31,29 @@ class MainListViewController: UIViewController {
 
 private extension MainListViewController {
     func setup() {
-        self.view.backgroundColor = .systemBackground
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension MainListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)"
+        cell.contentConfiguration = content
+        return cell
     }
 }
