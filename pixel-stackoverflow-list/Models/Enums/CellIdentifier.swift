3285693c449e28f72d31fd9e183265c462b5146d
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-05 10:39:16 +0100

    Create CellIdentifier.swift

---
+++ pixel-stackoverflow-list/Models/Enums/CellIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  CellIdentifier.swift
+//  pixel-stackoverflow-list
+//
+//  Created by Aleksandrs Trubacs on 05/09/2025.
+//
+
+import Foundation
+
+enum CellIdentifier: String {
+    case userViewCell = "UserViewCell"
+}
